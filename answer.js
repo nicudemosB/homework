72f4e3f72cbe43a797d0b357e99eb4e3e44c6563
Billyn <root@LAPTOP-EHBRACQA>
2022-07-13 22:39:13 -0400

    Methods Revisted answered

--- answer.js
+++ answer.js
@@ -33,11 +33,6 @@
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
-
-
-////////////////////////////////
-// Yell at the Ninja Turtles
-////////////////////////////////
 // const plantee = ["Plantee", "plant",  5000 , "Mordor"]
 //     plantee.pop(5000)
 //     plantee.push(5001)
@@ -61,11 +56,55 @@
     // console.log(wolfy);
     // not sure about this one
 
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+
+// for (const Donatello, Leonardo, Raphael, Michaelangeloiterator of object) {
+//     toUpperCase([0])
+//     console.log();
+    
+// }  
+
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// for (let i = 0; i < 9; i++) {
+//     console.log("Titanic");
+// }
+
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// favMovies.sort()
+//     console.log(favMovies); 
+    // yes the list of movies was altered
+    // const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+    // favMovies.pop()
+    // favMovies.push('Guardians of the Galaxy')
+    // console.log(favMovies);
+
+    // const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+    //         // favMovies.shift()
+    //         favMovies.unshift()
+    //         console.log(favMovies);
+            // jaws was shifted out from the beginning
 
 
+    // const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+    // favMovies.splice(4, 1)
+    // console.log(favMovies);
+        // not sure
+
+        // const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+        // favMovies.slice(3,9)
+        // console.log(favMovies.slice(3, 9));
+        // yes permenantly changed the array
+
+        //  since we did pop and push for Fast and the Furious to add Guardians of the Galaxy,
+        // we'll see that it replaced Fast and the Furious
+
+        // If the array changes then we don't need to use let
 
 ////////////////////////////////
 // Where is Waldo
