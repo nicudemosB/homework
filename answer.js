ede6be784a7204d3789cf2677545304b74d41d64
Billyn <root@LAPTOP-EHBRACQA>
2022-07-13 21:57:45 -0400

    Wild Wild Life answered

--- answer.js
+++ answer.js
@@ -18,18 +18,18 @@
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-for(i = 0; i <= 100; i++ ) {
-    if( i % 3 === 0 ); {
-        if (i % 5 === 0) {
-            if(i % 3 === 0 && i % 5 === 0);
+// for(i = 0; i <= 100; i++ ) {
+//     if( i % 3 === 0 ); {
+//         if (i % 5 === 0) {
+//             if(i % 3 === 0 && i % 5 === 0);
             
-        }
-        console.log('Fizz');
-        console.log('Buzz');
-        console.log('Fizz Buzz');
+//         }
+//         console.log('Fizz');
+//         console.log('Buzz');
+//         console.log('Fizz Buzz');
 
-    }
-}
+//     }
+// }
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
@@ -38,11 +38,33 @@ for(i = 0; i <= 100; i++ ) {
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+//     plantee.pop(5000)
+//     plantee.push(5001)
+//     console.log(Plantee)
+    
+//  
+    // don't get this part
 
+    // const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+    // wolfy.pop("Yukon Territory")
+    // wolfy.push("Gotham City")
+    // console.log(wolfy);
+
+    // const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+    // dart.push("Hawkins")
+    // console.log(dart);
+
+    // const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+    // wolfy.sort("Wolfy")
+    // wolfy.push("Gameboy")
+    // console.log(wolfy);
+    // not sure about this one
 
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+
 
 
 ////////////////////////////////
